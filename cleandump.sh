7b62fe09c0edc1f2e7e6be12b90c71a641ed7097
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2012-07-06 11:19:30 -0400

    Major rework to incluide loading file, use of eemasterconfig config.

--- cleandump.sh
+++ cleandump.sh
@@ -1,90 +1,187 @@
 #!/bin/bash
 
-# Check for existence of mysqldump and sed
+##########################################################
+## Define functions                                     ##
+##########################################################
+
+# Function to display usage
+usage() {
+  	cat <<HEREDOC
+	Usage:
+		`basename $0` (dump|load) [environment_shortname]
+
+	Options:
+		dump - Take a dump.
+		load - Load a dump.
+
+	Description:
+
+HEREDOC
+}
+
+# Function for creating dump files.
+dump() {
+	# # Create a dump of the database schema
+	# mysqldump \
+	## Connection to localhost
+	# 	--host=$HOST \
+	# 	--port=$PORT \
+	# 	--user=$USER \
+	# 	--password=$PASS \
+	## Drop everything first - Not drop database
+	##  for compatibility with cPanel
+	# 	--add-drop-table \
+	## Skip commenting, as timestamp changes diff
+	#  --skip-comments
+	## Make sure to recreate every detail
+	# 	--create-options \
+	## Use utf8
+	# 	--default-character-set=utf8 \
+	## Don't dump the data
+	# 	--no-data \
+	## Save db to schema.sql
+	# 	$DB >schema.sql
+
+	mysqldump --host="$HOST" --port="$PORT" --user="$USER" --password="$PASS" --add-drop-table --skip-comments --create-options --default-character-set=utf8 --no-data "$DB" >"$SCHEMA_FILE"
+
+	# # Create a dump of the actual data
+	# mysqldump \
+	## Connection to localhost
+	# 	--host=$HOST \
+	# 	--port=$PORT \
+	# 	--user=$USER \
+	# 	--password=$PASS \
+	## Include column names in inserts
+	# 	--complete-insert \
+	## Skip commenting, as timestamp changes diff
+	#  --skip-comments
+	## Make loading faster by delaying index creation
+	# 	--disable-keys \
+	## Use VALUES (row1), (row2),.. for smaller file, faster insert
+	# 	--extended-insert \
+	## Don't replicate db and table data
+	# 	--no-create-db \
+	# 	--no-create-info \
+	## Don't buffer rows in case db very large
+	# 	--quick \
+	##  Split VALUES (),(),...,(); into newline separated
+	##  This makes diffs smaller for git.
+	# 	$DB | sed 's/),(/),\
+	#   (/g'
+	## Save db to data.sql
+	# >data.sql
+
+	# Create a dump of the actual data
+	mysqldump --host="$HOST" --port="$PORT" --user="$USER" --password="$PASS" --complete-insert --skip-comments --disable-keys --extended-insert --no-create-db --no-create-info --quick "$DB" | sed 's/),(/),\
+	(/g' >"$DATA_FILE"
+}
+
+# Function for loading dump files.
+load() {
+	# Update the database
+	if [ -e "$SCHEMA_FILE" -a -e "$DATA_FILE" ]; then
+		mysql --user="$USER" --password="$PASS" --host="$HOST" --port="$PORT" "$DB" < "$SCHEMA_FILE"
+		mysql --user="$USER" --password="$PASS" --host="$HOST" --port="$PORT" "$DB" < "$DATA_FILE"
+	else
+		echo >&2 "One or both of the schema and data files for this environment do not exist."; exit 1;
+	fi
+}
+
+##########################################################
+## Checks before we do any more                         ##
+##########################################################
+
+# Check for existence/accessibility of mysql and sed
+hash mysql 2>&- || { echo >&2 "Mysql is required but is either not installed or not in your PATH.  Aborting."; exit 1; }
 hash mysqldump 2>&- || { echo >&2 "The mysqldump utility is required but is either not installed or not in your PATH.  Aborting."; exit 1; }
 hash sed 2>&- || { echo >&2 "The sed utility is required but is either not installed or not in your PATH.  Aborting."; exit 1; }
 
-# Check that user supplied a conf file
+# Check that user is asking for an action
 if [ ! $1 ]; then
-  	cat <<HEREDOC
-MySQL CleanDump v1.0.0
-	Usage:
-		Specify a configuration file for the environment you are in as an arguement.
-
-	Example:
-		cleandump.sh ./env.local.conf
-HEREDOC
+	usage
   	exit;
 fi
 
-# Set defaults
-ENV="local"
-HOST="localhost"
-PORT="3306"
-USER="root"
-PASS="root"
-DB="test"
-
-# Check if conf exists, then load
-if [ -e $1 ]; then
-	source "$1"
-else
-	echo >&2 "Configuration file does not exist."; exit 1;
-fi
+###########################################################
+## Set global variables and grab configuration           ##
+###########################################################
 
 # Find location of this script so we can save output to correct directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# # Create a dump of the database schema
-# mysqldump \
-## Connection to localhost
-# 	--host=$HOST \
-# 	--port=$PORT \
-# 	--user=$USER \
-# 	--password=$PASS \
-## Drop everything first - Not drop database
-##  for compatibility with cPanel
-# 	--add-drop-table \
-## Skip commenting, as timestamp changes diff
-#  --skip-comments
-## Make sure to recreate every detail
-# 	--create-options \
-## Use utf8
-# 	--default-character-set=utf8 \
-## Don't dump the data
-# 	--no-data \
-## Save db to schema.sql
-# 	$DB >schema.sql
+# Check that user supplied an env -- falback to local
+if [ ! $2 ]; then
+	ENV="local"
+else
+	ENV="$2"
+fi
 
-mysqldump --host="$HOST" --port="$PORT" --user="$USER" --password="$PASS" --add-drop-table --skip-comments --create-options --default-character-set=utf8 --no-data "$DB" >"$DIR"/"$ENV"-schema.sql
+# Set defaults to prevent nonexistence errors.
+DEFAULT_HOST="localhost"
+DEFAULT_PORT="3306"
+DEFAULT_USER="root"
+DEFAULT_PASS="root"
+DEFAULT_DB="test"
 
-# # Create a dump of the actual data
-# mysqldump \
-## Connection to localhost
-# 	--host=$HOST \
-# 	--port=$PORT \
-# 	--user=$USER \
-# 	--password=$PASS \
-## Include column names in inserts
-# 	--complete-insert \
-## Skip commenting, as timestamp changes diff
-#  --skip-comments
-## Make loading faster by delaying index creation
-# 	--disable-keys \
-## Use VALUES (row1), (row2),.. for smaller file, faster insert
-# 	--extended-insert \
-## Don't replicate db and table data
-# 	--no-create-db \
-# 	--no-create-info \
-## Don't buffer rows in case db very large
-# 	--quick \
-##  Split VALUES (),(),...,(); into newline separated
-##  This makes diffs smaller for git.
-# 	$DB | sed 's/),(/),\
-#   (/g'
-## Save db to data.sql
-# >data.sql
+HOST="$DEFAULT_HOST"
+PORT="$DEFAULT_PORT"
+USER="$DEFAULT_USER"
+PASS="$DEFAULT_PASS"
+DB="$DEFAULT_DB"
 
-# Create a dump of the actual data
-mysqldump --host="$HOST" --port="$PORT" --user="$USER" --password="$PASS" --complete-insert --skip-comments --disable-keys --extended-insert --no-create-db --no-create-info --quick "$DB" | sed 's/),(/),\
-(/g' >"$DIR"/"$ENV"-data.sql
+# Path to possible configuration files
+EEMASTERCONF_FILE="$DIR/../config/config.$ENV.php"
+CONF_FILE="$DIR/$ENV.conf"
+
+## Grab configuration
+if [ -e "$EEMASTERCONF_FILE" ]; then
+# Check if ee-master-config exists for this env, then pull form that
+	HOST=`grep -Pio "(?<=hostname\'\]\ \=\ \')([a-z\@\#%^&*()_+.:]+)(?=\')" "$EEMASTERCONF_FILE"`
+	PORT=`grep -Pio "(?<=port\'\]\ \=\ \')([a-z\@\#%^&*()_+.:]+)(?=\')" "$EEMASTERCONF_FILE"`
+	USER=`grep -Pio "(?<=username\'\]\ \=\ \')([a-z\@\#%^&*()_+.:]+)(?=\')" "$EEMASTERCONF_FILE"`
+	PASS=`grep -Pio "(?<=password\'\]\ \=\ \')([a-z\@\#%^&*()_+.:]+)(?=\')" "$EEMASTERCONF_FILE"`
+	DB=`grep -Pio "(?<=database\'\]\ \=\ \')([a-z\@\#%^&*()_+.:]+)(?=\')" "$EEMASTERCONF_FILE"`
+
+	for VAR in HOST PORT USER PASS DB; do
+		if [ "${!VAR}" = "" ]; then
+			eval $VAR="${!DEFAULT_$VAR}"
+		fi
+	done
+elif [ -e "$CONF_FILE" ]; then
+# Check for configuration file as fallback.
+	source "$CONF_FILE"
+else
+# Error out.
+	echo >&2 "Configuration file does not exist."; exit 1;
+fi
+
+# Now that we have complete config data, set locations of dump files.
+DATA_FILE="$DIR/$ENV-data.sql"
+SCHEMA_FILE="$DIR/$ENV-schema.sql"
+
+
+###########################################################
+## Main logic.                                           ##
+###########################################################
+
+case $1 in
+	"load")
+		load
+		;;
+	"dump")
+		dump
+		;;
+	"debug")
+		echo "HOST: $HOST"
+		echo "PORT: $PORT"
+		echo "USER: $USER"
+		echo "PASS: $PASS"
+		echo "DB: $DB"
+		echo "DATA_FILE: $DATA_FILE"
+		echo "SCHEMA_FILE: $SCHEMA_FILE"
+		echo "EEMASTERCONF_FILE: $EEMASTERCONF_FILE"
+		echo "CONF_FILE: $CONF_FILE"
+	*)
+		usage
+		;;
+esac
